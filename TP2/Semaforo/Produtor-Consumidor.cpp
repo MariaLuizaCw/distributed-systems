cbda1286f1925a005b79e16a6c302272afcfd53a
root <root@DESKTOP-O1197EE>
2023-05-16 20:57:44 -0300

    add buffer occupation graphs

--- TP2/Semaforo/Produtor-Consumidor.cpp
+++ TP2/Semaforo/Produtor-Consumidor.cpp
@@ -4,10 +4,12 @@
 #include <thread>
 #include <queue>
 #include <time.h>
+#include <fstream>  
 
 using namespace std;
 
-
+ofstream arq;    
+struct timespec start, finish;
 sem_t full;
 sem_t empty_mem;
 int num_val_processar = 100000;
@@ -29,30 +31,44 @@ int is_prime(int n)
     return 1;
 }
 
+void buffer_allocationn(){
+    struct timespec buffer_end_time;
+    clock_gettime(CLOCK_MONOTONIC, &buffer_end_time);
+
+    double buffer_state_time = (buffer_end_time.tv_sec - start.tv_sec);
+    buffer_state_time += (buffer_end_time.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    int size = 0;
+    for(int i = 0; i <= tamanho_mem; i++){
+        if(memoria[i] != 0)
+            size += 1;             
+    }
+    arq << buffer_state_time << ',' << size << '\n';
+}
+
 
 void produtor()
 {
     unsigned seed = time(0);
     srand(seed);
     int numMax = pow(10, 7) - 1;    
-
     while(num_val_processar > 0)
     {
         int valor_aleatorio = 1 + rand() % numMax;
         sem_wait(&empty_mem);
         sem_wait(&mutex_mem);
-
             auto posicao = find(memoria.begin(), memoria.end(), 0);
             if (posicao != memoria.end()) {
                 int pos = posicao - memoria.begin();
                 memoria[pos] = valor_aleatorio;
             }
-        
+            // buffer_allocationn();
         sem_post(&mutex_mem);
         sem_post(&full);
     }
     sem_post(&full);
     sem_post(&empty_mem);
+    
 }
 
 
@@ -62,14 +78,16 @@ void consumidor()
     while(num_val_processar > 0) {
         sem_wait(&full);
         sem_wait(&mutex_mem);
-            auto posicao = find_if(memoria.begin(), memoria.end(),[](int x) { return x != 0; });
 
+           
+            auto posicao = find_if(memoria.begin(), memoria.end(),[](int x) { return x != 0; });
             if (posicao != memoria.end()) {    
                 int pos = posicao - memoria.begin();
                 valor_recolhido = memoria[pos];
                 memoria[pos] = 0;
             }    
 
+            // buffer_allocationn();
             num_val_processar--;
         sem_post(&mutex_mem);
         sem_post(&empty_mem);
@@ -98,11 +116,12 @@ int main(int argc, char* argv[])
     sem_init(&mutex_mem,0,1);
     
     vector<thread> threads;
-
-    struct timespec start, finish;
+    // arq.open("results/test_" + to_string(num_th_prod) + '_' + to_string(num_th_consum) + '_' + to_string(tamanho_mem) + ".txt");
+    // arq << "time,buffersize" << '\n';
     double elapsed;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
+
 
     for (int i = 0; i < num_th_prod; i++) threads.push_back(thread(produtor));
     for (int i = 0; i < num_th_consum; i++) threads.push_back(thread(consumidor));
@@ -110,7 +129,7 @@ int main(int argc, char* argv[])
     for (auto& th : threads) th.join();
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
-
+    // arq.close();
     elapsed = (finish.tv_sec - start.tv_sec);
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
     cout << "Rodou todos os testes\n";

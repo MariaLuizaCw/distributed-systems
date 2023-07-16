ce1ff4e9aade18549de8f2a719f589cab45f08a6
Yasmin Forestti <87791176+YasminForestti@users.noreply.github.com>
2023-07-16 16:33:31 -0300

    add getpid() in client

--- TP3/client.cpp
+++ TP3/client.cpp
@@ -27,14 +27,18 @@ string CurrentTime()
 
     // Remover o caractere de nova linha da string
     time_string.erase(time_string.length() - 1);
-
+    tm timeInfo;
+    strptime(time_string.c_str(), "%a %b %e %H:%M:%S %Y", &timeInfo);
+    char timeString[9];
+    strftime(timeString, sizeof(timeString), "%T", &timeInfo);
+    string timeFormatted(timeString);
     // Converter o valor dos milissegundos para string
     string ms = to_string(value % 1000);
 
     // Formatando a string com milissegundos
-    time_string += "." + string(3 - ms.length(), '0') + ms;
+    timeFormatted += ":" + string(3 - ms.length(), '0') + ms;
     cout << "end CurrentTime" << endl;
-    return time_string;
+    return timeFormatted;
 }
 
 void generate_message(int code, int id, char message[10]) {
@@ -96,7 +100,7 @@ int main(int argc, char* argv[])
     for(int i =0; i < 10; i++){
 
         char request_msg[10];
-        generate_message(1, connreq, request_msg);
+        generate_message(1, getpid(), request_msg);
 
         cout << request_msg << '\n';
         int send_resquest_to_server = send(csock, request_msg, 10, 0); 
@@ -112,7 +116,7 @@ int main(int argc, char* argv[])
             // sleep(4);
             
             char release_msg[10];
-            generate_message(3, connreq, release_msg);
+            generate_message(3, getpid(), release_msg);
             cout << release_msg << '\n';
 
             int send_response_to_server = send(csock, release_msg, 10, 0);;
